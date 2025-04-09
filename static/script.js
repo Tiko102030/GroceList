1b4670821a3bc441e1a525a04dc988d354eee631
Tiko <Tikhon102030@gmail.com>
2025-04-08 18:03:30 -0700

    Full remake

--- static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".remove-btn").forEach(button => {
-        button.addEventListener("click", () => {
-            let productId = button.getAttribute("data-id");
-            removeProduct(productId);
-        });
-    });
-});
